79ef19d47c1fd0cc89b2db6379b5c35a3d2bb37c
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-06-05 20:49:49 +0530

    First commit: create the directory structure and setup the express server

---
+++ index.js
@@ -0,0 +1,17 @@
+//  Step-1 :- importing and initializing express and port
+const express = require("express");
+const port = 8000;
+
+// Step-2 :- Starting or firingup our express server...
+const app = express();
+
+// Step-3 :- Now we'hv to make our server listen to that port...
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`There is some ${err} in listening with that port...`);
+  } else {
+    console.log(
+      `Successfully, your server is listening and it is running live on port : ${port}`
+    );
+  }
+});
