1ff001447335c3612cded834e6752415f06cd6ec
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-06-06 19:48:12 +0530

    Added a controller and a router for user profile...

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ console.log("Router has been loaded successfully...");
 const homeController = require("../controllers/home_controller");
 
 router.get("/", homeController.home);
+router.use("/users", require("./users"));
+
+// For any further routes, access from here...
+// router.use("/routeName", require("./routeFile"));
 
 module.exports = router;
