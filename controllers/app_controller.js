3d61b655e0cf23d09c6dab807583db6a9861ae13
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-06-07 18:04:17 +0530

    created a app setting view file and partials for all...

--- controllers/app_controller.js
+++ controllers/app_controller.js
@@ -1,3 +1,5 @@
 module.exports.setting = (req, res) => {
-    res.end("<h1>This is your App setting through which you can customize your app as per your wish...</h1>");
-}
+  return res.render("app", {
+    title: "App_Settings",
+  });
+};
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,10 @@
 module.exports.profile = (req, res) => {
-  res.render("user_file", {
+  return res.render("user_file", {
     title: "user_profile_page",
   });
 };
 module.exports.setting = (req, res) => {
-  res.render("user_file", {
+  return res.render("user_file", {
     title: "users_setting_page",
   });
 };
